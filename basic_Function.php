b0579443ff91061e4fb4abc5cac4818befecf995
mosfeqanik <mosfeqanik01@gmail.com>
2019-01-24 23:02:08 +0600

    by reference added

--- basic_Function.php
+++ basic_Function.php
@@ -19,16 +19,35 @@ simpleFunction();
 // function with param
 function sayhello($name)
 {
-	echo "Hello <br>";
+	echo "Hello $name<br>";
 }
 sayhello("joe");
 sayhello("bob");
 
+function addnumbers($num1,$num2)
+{
+	return $num1+$num2;
+}
+
+echo addnumbers(12,25)."<br>";
 
 
+// BY reference
+$mynum=10;
+function addfive($num)
+{
+	$num +=5;
+}
+function addten(&$num)
+{
+	$num +=10;
+}
 
+addfive($mynum);
+echo "value: $mynum<br>";
 
-
+addten($mynum);
+echo "value: $mynum<br>";
 
 
 
