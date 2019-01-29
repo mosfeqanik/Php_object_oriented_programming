348fc3bd9c78aa13cdfeb27c1163cb578c3583bc
mosfeqanik <mosfeqanik01@gmail.com>
2019-01-29 22:41:47 +0600

    mystatus assignment completed

--- MyStatus.php
+++ MyStatus.php
@@ -1,1 +1,20 @@
-MyStatus.php
+<?php 
+include "Gorib.php";
+class MyStatus implements Gorib
+{
+	public function lowMOney()
+	{
+		echo "i am for lowmoney <br>";
+	}
+	public function Homeless()
+	{
+		echo "i am for lowmoney because i am homeless <br>";
+	}
+}
+
+$bank=new MyStatus();
+$bank->lowMOney();
+$bank->Homeless()
+
+
+?>
