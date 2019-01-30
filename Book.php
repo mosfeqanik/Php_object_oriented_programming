47144b7a98c5770892ca683e77a762e7933decf5
mosfeqanik <mosfeqanik01@gmail.com>
2019-01-30 19:07:21 +0600

    init class updated

--- Book.php
+++ Book.php
@@ -43,20 +43,6 @@ class Book
 	}
 
 }
-$harry_potter=new Book(3945664,"harry potter and the philosopher's stone","J. K. Rowling",12);
-echo $harry_potter;
-
-if ($harry_potter->getcopy()) 
-{
-	echo "here is your copy";
-}
-else
-{
-	echo "sorry  there is no copy";
-}
-
-// var_dump(Book);
-var_dump($harry_potter);
 
 
 
--- init.php
+++ init.php
@@ -1,1 +1,33 @@
-init.php
+<?php  
+
+
+
+include 'Book.php';
+include 'Customer.php';
+
+$harry_potter=new Book(3945664,"harry potter and the philosopher's stone","J. K. Rowling",12);
+echo $harry_potter;
+
+if ($harry_potter->getcopy()) 
+{
+	echo "here is your copy";
+}
+else
+{
+	echo "sorry  there is no copy";
+}
+
+// var_dump(Book);
+var_dump($harry_potter);
+
+
+
+
+
+
+
+
+
+
+
+?>
