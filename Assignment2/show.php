4333f48a388c30350ca344c8306d431d64d2ac86
mosfeqanik <mosfeqanik01@gmail.com>
2019-02-11 21:43:28 +0600

    Assignment is completed

--- Assignment2/show.php
+++ Assignment2/show.php
@@ -4,14 +4,42 @@ include 'Database.php';
 $show = new Database();
 $data= $show->read();
 print_r($data);
-foreach ($data as $key => $value) 
-{
-	echo $value["name"]."<br>";
-	echo $value["model"]."<br>";
-	echo $value["price"]."<br>";
+// foreach ($data as $key => $value) 
+// {
+// 	echo $value["name"]."<br>";
+// 	echo $value["email"]."<br>";
+// 	echo $value["phone"]."<br>";
 
-}
+// }
 
 
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>show details</title>
+</head>
+<body>
+	<table border="2px solid black">
+		<tr>
+			<th>Name</th>
+			<th>Email</th>
+			<th>Phone</th>
+		</tr>
+		<?php
+			foreach ($data as $key =>$value ) {
+			
+		?>
+		<tr>
+			<td><?php  echo $value['name'];?></td>
+			<td><?php  echo $value['email'];?></td>
+			<td><?php  echo $value['phone'];?></td>
+		</tr>
+		<?php
+		
+			}	
+		?>
+	</table>
+</body>
+</html>
