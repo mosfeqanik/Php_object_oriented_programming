0e9956cc7271f87fe768e6e02d3e98be102d0bec
mosfeqanik <mosfeqanik01@gmail.com>
2019-02-11 19:54:52 +0600

    Assignment File is created

--- 10_2_2019/Show.php
+++ 10_2_2019/Show.php
@@ -4,14 +4,43 @@ include 'Db.php';
 $shw = new Db();
 $data= $shw->Show();
 // print_r($data);
-foreach ($data as $key => $value) 
-{
-	echo $value["name"]."<br>";
-	echo $value["model"]."<br>";
-	echo $value["price"]."<br>";
+// foreach ($data as $key => $value) 
+// {
+// 	echo $value["name"]."<br>";
+// 	echo $value["model"]."<br>";
+// 	echo $value["price"]."<br>";
 
-}
+// }
 
 
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>show details</title>
+</head>
+<body>
+	<table border="2px solid black">
+		<tr>
+			<th>Name</th>
+			<th>Model</th>
+			<th>Price</th>
+		</tr>
+		<?php
+			foreach ($data as $key =>$value ) {
+			
+		?>
+		<tr>
+			<td><?php  echo $value['name'];?></td>
+			<td><?php  echo $value['model'];?></td>
+			<td><?php  echo $value['price'];?></td>
+		</tr>
+		<?php
+		
+			}	
+		?>
+	</table>
+</body>
+</html>
+
---
+++ Assignment2/Crud.php
@@ -0,0 +1,17 @@
+<?php  
+
+interface Crud
+{
+
+
+	public function save();
+	public function read();
+	
+}
+
+
+
+
+
+
+?>
