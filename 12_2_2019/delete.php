af734100f510a1edb2547bcef53ca3768823a701
mosfeqanik <mosfeqanik01@gmail.com>
2019-02-13 10:45:00 +0600

    deleting by id is created

--- 12_2_2019/delete.php
+++ 12_2_2019/delete.php
@@ -1,1 +1,14 @@
-delete.php
+<?php  
+
+include 'Db.php';
+$del = new Db();
+if(isset($_GET["id"]))
+	{	
+		$id=$_GET["id"];
+		$result=$del->Delete($id);
+		header( "location:Show.php");
+
+	}
+
+
+?>
